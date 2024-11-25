07f4778bcffc433ae6aa21d9a56a6edd04a20970
Anh Duc <anhducnguyensy06@gmail.com>
2024-11-25 14:42:22 +0700

    first commit

---
+++ Lab03/CodeJava/AimsProject/src/Cart.java
@@ -0,0 +1,44 @@
+public class Cart {
+    public static final int MAX_NUMBER_ORDER = 20;
+
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDER];
+
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBER_ORDER) {
+            itemOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added.");
+        } else {
+            System.out.println("The cart is almost full.");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemOrdered[i].equals(disc)) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemOrdered[j] = itemOrdered[j + 1];
+                }
+                itemOrdered[qtyOrdered - 1] = null; 
+                qtyOrdered--;
+                found = true;
+                System.out.println("The disc has been removed.");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("The disc is not found in the cart.");
+        }
+    }
+
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemOrdered[i].getCost();
+        }
+        return total;
+    }
+}
